554c03d9a1c65fcd1396f4e9c16e33e08e422cc5
Pedrinnnnn <77684423+PedrinhoDBR@users.noreply.github.com>
2026-05-07 23:58:11 -0300

    feat: portifolio

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ra2581392523016
-Repositório do aluno PEDRO AUGUSTO GOMES.
+# Portfólio de Projetos - Fatec Jacareí
+### Aluno: Pedro Augusto Gomes
+### Curso: Desenvolvimento de Software Multiplataforma
+### Turma: 2° Semestre de 2025
+ 
+---
+
+### Portfólio Online  
+🔗 [Acesse o Portfólio](https://fatec-jacarei-dsm-portfolio.github.io/ra2581392523016/)
+🎤 [Vídeo de Apresentação]()
---
+++ docs/js/scripts.js
@@ -0,0 +1,63 @@
+function switchLanguage(lang) {
+    document.querySelectorAll('[data-key]').forEach(el => {
+        const key = el.getAttribute('data-key');
+        el.textContent = translations[lang][key];
+    });
+
+    document.querySelectorAll('.lang-button').forEach(button => {
+        button.classList.remove('active');
+
+        const buttonLang = button.textContent.trim().toLowerCase();
+
+        if (buttonLang === lang) {
+            button.classList.add('active');
+        }
+    });
+    
+    commandText = ' ' + translations[lang].thanksCommand;
+    resetTypingAnimation();
+
+    localStorage.setItem('lang', lang);
+}
+
+fetch('/docs/locale/translations.json')
+  .then(res => res.json())
+  .then(data => {
+      window.translations = data;
+
+      const savedLang = localStorage.getItem('lang') || 'pt';
+      switchLanguage(savedLang);
+  });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.animationDelay = '0.1s';
+        }
+    });
+});
+
+document.querySelectorAll('.section').forEach(section => {
+    observer.observe(section);
+});
+
+const commandElement = document.querySelector('.command-line');
+
+let commandText = '';
+let currentText = '';
+let index = 0;
+
+function typeCommand() {
+    if (index < commandText.length) {
+        currentText += commandText[index];
+        commandElement.innerHTML = currentText + '<span class="cursor"></span>';
+        index++;
+        setTimeout(typeCommand, 50);
+    }
+}
+
+function resetTypingAnimation() {
+    currentText = commandElement.innerHTML.split('$')[0] + '$';
+    index = 0;
+    typeCommand();
+}
